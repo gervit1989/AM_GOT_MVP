8c7107e7b5c43c7c75fa5427f855bf7e0afc0e6c
gervit1989 <gervit2008@mail.ru>
2016-10-25 07:22:24 +0300

    Initial commit

---
+++ app/src/main/java/com/skillbranch/sb_got_mvp/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.skillbranch.sb_got_mvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.skillbranch.devtest.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
